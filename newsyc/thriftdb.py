31b9beb7a9efc7559f70f596504f7e8b56003656
Zach Williams <hey@zachwill.com>
2012-05-27 15:18:17 -0700

    Will need to normalize ThriftDB parameters

---
+++ newsyc/thriftdb.py
@@ -0,0 +1,11 @@
+"""
+ThriftDB API:  http://www.thriftdb.com/documentation/rest-api/search-api
+
+Turn a dictionary of sane key-value pairs to the somewhat crazy ThriftDB
+key-value parameters.
+"""
+
+
+def convert(data):
+    """Where the conversion magic happens."""
+    return data
