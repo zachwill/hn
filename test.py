05859b2e51871058c3f6b93c98e5ff5867e9bc0e
Zach Williams <hey@zachwill.com>
2012-05-28 15:53:16 -0700

    Begin adding search tests

--- test.py
+++ test.py
@@ -21,28 +21,61 @@ class CLI(TestCase):
         self.assertTrue(isinstance(options.search, list))
 
     def test_parser_can_handle_multiple_search_terms(self):
-        args = ['one', 'two', 'three']
+        args = 'one two three'.split(' ')
         options = self.parser.parse_args(args)
         self.assertEquals(options.search, args)
 
     def test_parser_recognizes_limit_and_start_options(self):
-        args = ['term', '-n', '50', '-s', '100']
+        args = 'term -n 50 -s 100'.split(' ')
         options = self.parser.parse_args(args)
         self.assertEquals(options.limit, 50)
         self.assertEquals(options.start, 100)
 
     def test_parser_can_recognize_dates(self):
-        args = ['', '-d', '03-16-12']
+        args = '-d 03-16-12'.split(' ')
         options = self.parser.parse_args(args)
         self.assertEquals(options.day, '03-16-12')
-        args = ['', '-d', '2012-03-16']
+        args = '-d 2012-03-16'.split(' ')
         options = self.parser.parse_args(args)
         self.assertEquals(options.day, '2012-03-16')
 
     def test_parser_recognizes_hits_flag(self):
-        args = ['github', '--hits']
+        args = 'github --hits'.split(' ')
         options = self.parser.parse_args(args)
         self.assertTrue(options.hits)
+
+
+class Search(TestCase):
+
+    def setUp(self):
+        cli.News = Mock()
+        core.req = Mock()
+        core.json = Mock()
+        cli.present = Mock()
+
+    def create_cli(self, args):
+        parser = cli.create_parser()
+        options = parser.parse_args(args.split(' '))
+        cli.search(options)
+
+    def assertSearchParameters(self, **params):
+        cli.News().get.assert_called_with(**params)
+
+    def test_basic_search_term(self):
+        self.create_cli('zachwill')
+        self.assertSearchParameters(q='zachwill')
+
+    def test_multiple_search_terms(self):
+        self.create_cli('one two three')
+        self.assertSearchParameters(q='one two three')
+
+    def test_limit_and_start_options(self):
+        self.create_cli('github -n 100 -s 200')
+        self.assertSearchParameters(q='github', limit=100, start=200)
+
+    def test_sortby_parameter(self):
+        self.create_cli('pg -S points')
+        self.assertSearchParameters(q='pg', sortby='points desc')
 
 
 if __name__ == '__main__':
